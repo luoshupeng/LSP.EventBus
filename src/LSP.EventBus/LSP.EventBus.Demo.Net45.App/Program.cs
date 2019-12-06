d4e918058de8a0d50bb49b87123dbd9491f721c7
luoshupeng <shupengluo@gmail.com>
2019-12-06 08:47:30 +0800

    添加示例代码

---
+++ src/LSP.EventBus/LSP.EventBus.Demo.Net45.App/Program.cs
@@ -0,0 +1,42 @@
+﻿using LSP.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP.EventBus.Demo.Net45.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventBusFactory.DefaultEventBus.Subscribe<PayloadEvent<int>>("default", OnIntEvent);
+            EventBusFactory.DefaultEventBus.Subscribe<CustomEvent>("custom", OnCustomEvent);
+
+            EventBusFactory.DefaultEventBus.Publish("default", new PayloadEvent<int>(1234));
+            EventBusFactory.DefaultEventBus.Publish("custom", new CustomEvent());
+
+            EventBusFactory.DefaultEventBus.Subscribe<PayloadEvent<string>>("new", s => {
+                Console.WriteLine(s.Payload);
+            });
+            EventBusFactory.DefaultEventBus.Publish("new", new PayloadEvent<string>("this is new message"));
+
+            Console.ReadKey();
+        }
+
+        public static void OnIntEvent(PayloadEvent<int> intEvent)
+        {
+            Console.WriteLine(intEvent.Payload);
+        }
+
+        public static void OnCustomEvent(CustomEvent customEvent)
+        {
+            Console.WriteLine("Received Custom Event");
+        }
+    }
+
+    class CustomEvent : EventBase
+    {
+    }
+}
---
+++ src/LSP.EventBus/LSP.EventBus.Demo.Netcore31.App/Program.cs
@@ -0,0 +1,39 @@
+﻿using LSP.EventBus.Events;
+using System;
+
+namespace LSP.EventBus.Demo.Netcore31.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            EventBusFactory.DefaultEventBus.Subscribe<PayloadEvent<int>>("default", OnIntEvent);
+            EventBusFactory.DefaultEventBus.Subscribe<CustomEvent>("custom", OnCustomEvent);
+
+            EventBusFactory.DefaultEventBus.Publish("default", new PayloadEvent<int>(1234));
+            EventBusFactory.DefaultEventBus.Publish("custom", new CustomEvent());
+
+            EventBusFactory.DefaultEventBus.Subscribe<PayloadEvent<string>>("new", s => {
+                Console.WriteLine(s.Payload);
+            });
+            EventBusFactory.DefaultEventBus.Publish("new", new PayloadEvent<string>("this is new message"));
+
+            Console.ReadKey();
+        }
+        public static void OnIntEvent(PayloadEvent<int> intEvent)
+        {
+            Console.WriteLine(intEvent.Payload);
+        }
+
+        public static void OnCustomEvent(CustomEvent customEvent)
+        {
+            Console.WriteLine("Received Custom Event");
+        }
+    }
+
+    class CustomEvent : EventBase
+    {
+    }
+}
